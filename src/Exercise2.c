9c0a863a881a7acc69777683f523da87f27b2f52
ITITIU20264 <ITITIU20264@student.hcmiu.edu.vn>
2021-11-22 17:20:40 +0700

    Exercise for lab 6

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -12,27 +12,33 @@ Ex:
 #include <math.h>
 #include <string.h>
 
+int compare(char a[], char b[]){
+    int i = 0, k = 0;
+
+    while(a[i] != '\0' && b[i] != '\0')  {  
+        if(a[i] > b[i]) return 1; 
+        if(a[i] < b[i]) return -1;
+        i++;
+    }  
+
+}
+
 void Ex2(int n, char *str[]){
-	//Your codes here
-	char c[1000];
-	int i, j;
-    for (i = 0; i < n; i++) 
-	{
-        for (j = i + 1; j < n; j++) 
-		{
-            if (strcmp(str[i], str[j]) > 0) 
-			{
-                strcpy(c, str[i]);
+
+    char temp[10];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (compare(str[i], str[j]) > 0) {
+                strcpy(temp, str[i]);
                 strcpy(str[i], str[j]);
-                strcpy(str[j], c);
+                strcpy(str[j], temp);
             }
         }
     }
-    for(i = 0; i < n; i++) 
-	{
-		printf("%s ",str[i]);
-	}	
+    for(int i = 0; i < n; i++) printf("%s ",str[i]);
 }
+
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
