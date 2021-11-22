6f6e6ff165be01be3e5d5353d96f64877691ee76
ITITIU20264 <ITITIU20264@student.hcmiu.edu.vn>
2021-11-22 16:22:21 +0700

    Exercise for lab 6

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,10 +12,47 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
-void Ex3(char *str){
+void Ex3(char *c){
 	//Your codes here
-	
+	char input[1000] = "";
+    char word[100] = "";
+    char longestWord[100] = "";
+    char shortestWord[100] = "";
+    int wordIndex = 0;
+    printf("Input a string: ");
+    fgets(input, sizeof(input), stdin);
+    for(int inputIndex = 0; inputIndex < strlen(input); inputIndex++)
+    {
+        while (inputIndex < strlen(input) && !isspace(input[inputIndex]) && isalnum(input[inputIndex]))
+        {
+            word[wordIndex++] = input[inputIndex++];
+        }
+        if(wordIndex != 0)
+        {
+            word[wordIndex] = '\0';
+        }
+        if(strlen(longestWord) == 0)
+        {
+            strcpy(longestWord, word);
+        }
+        if(strlen(shortestWord) == 0)
+        {
+            strcpy(shortestWord, word);
+        }
+        if(strlen(word) > strlen(longestWord))
+        {
+            strcpy(longestWord, word);
+        }
+        if(strlen(word) < strlen(shortestWord))
+        {
+            strcpy(shortestWord, word);
+        }
+        wordIndex = 0;
+    }
+    printf("Longest word: \'%s\'", longestWord);
+    printf("\nShortest word: \'%s\'", shortestWord);
 }
 
 int main(int argc, char *argv[]) {
